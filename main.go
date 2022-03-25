8cb069950f11ec5a9a36cd981e6eff6c66540e23
Siyovush <paraparadox@protonmail.com>
2022-03-25 21:20:38 +0500

    Stage 2/4. Core functionality in In-Memory-Notepad

--- main.go
+++ main.go
@@ -5,22 +5,49 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	const n = 5
+	var i int
+	var notes [n]string
 	for {
 		fmt.Print("Enter command and data: ")
 		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Split(bufio.ScanWords)
 		scanner.Scan()
-		command := scanner.Text()
+		line := scanner.Text()
+		commandAndData := strings.SplitN(line, " ", 2)
+		command := commandAndData[0]
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
 		if command == "exit" {
 			break
 		}
-		fmt.Println(command)
+		switch command {
+		case "create":
+			if i == n {
+				fmt.Println("[Error] Notepad is full")
+			} else {
+				data := commandAndData[1]
+				notes[i] = data
+				i++
+				fmt.Println("[OK] The note was successfully created")
+			}
+		case "list":
+			for j := 0; j < n; j++ {
+				if notes[j] != "" {
+					fmt.Printf("[INFO] %d: %s\n", j+1, notes[j])
+				}
+			}
+		case "clear":
+			for j := 0; j < n; j++ {
+				notes[j] = ""
+			}
+			i = 0
+			fmt.Println("[OK] All notes were successfully deleted")
+		}
 	}
 	fmt.Println("[Info] Bye!")
 }
