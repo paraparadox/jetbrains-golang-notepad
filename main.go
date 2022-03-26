de9f0e15b17d6b719af871e4fcb5d9636b3a215e
Siyovush <paraparadox@protonmail.com>
2022-03-26 16:12:57 +0500

    Stage 4/4: Update command

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -55,6 +56,33 @@ func main() {
 			}
 			i = 0
 			fmt.Println("[OK] All notes were successfully deleted")
+		case "update":
+			input := strings.Split(line, " ")
+			if len(input) < 2 || input[1] == "" {
+				fmt.Println("[Error] Missing position argument")
+				break
+			}
+			if len(input) < 3 || input[2] == "" {
+				fmt.Println("[Error] Missing note argument")
+				break
+			}
+			numberAndData := strings.SplitN(commandAndData[1], " ", 2)
+			numberStr := numberAndData[0]
+			number, err := strconv.ParseInt(numberStr, 10, 64)
+			if err != nil {
+				fmt.Printf("[Error] Invalid position: %s\n", numberStr)
+				break
+			}
+			if number < 1 || number > int64(n) {
+				fmt.Printf("[Error] Position %d is out of the boundaries [1, %d]\n", number, n)
+				break
+			}
+			if notes[number-1] == "" {
+				fmt.Println("[Error] There is nothing to update")
+				break
+			}
+			notes[number-1] = numberAndData[1]
+			fmt.Printf("[OK] The note at position %d was successfully updated\n", number)
 		default:
 			fmt.Println("[Error] Unknown command")
 		}
